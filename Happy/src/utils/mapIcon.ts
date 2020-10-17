bc00bf91956bd8699c10084fb5875ab3b6abeaee
Sylvn001 <juniorbaarros@gmail.com>
2020-10-17 14:11:55 -0300

    Day 3 complete! Project completed!

---
+++ Happy/src/utils/mapIcon.ts
@@ -0,0 +1,13 @@
+import Leaflet from 'leaflet'
+
+import mapMakerImg from '../imagens/Map.svg'
+
+const mapIcon = Leaflet.icon({
+    iconUrl: mapMakerImg,
+  
+    iconSize: [58, 68],
+    iconAnchor: [29, 68],
+    popupAnchor: [0, -60]
+  })
+
+  export default mapIcon;
